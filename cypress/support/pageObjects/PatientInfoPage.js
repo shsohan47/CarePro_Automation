3d7af93bbb2eac3e078308b5714a05c61443cc40
sohanpro <shsohan47@gmail.com>
2024-05-20 21:40:58 +0600

    :zap: patient info selectors done

---
+++ cypress/support/pageObjects/PatientInfoPage.js
@@ -0,0 +1,21 @@
+export default class PatientInfo{
+    selectors={
+        date:":nth-child(1) > .flex-col > .custom-input'",
+        time:":nth-child(2) > .flex-col > .custom-input'",
+        weight:':nth-child(3) > .flex-col > .custom-input',
+        height:':nth-child(4) > .flex-col > .custom-input',
+        temp:':nth-child(5) > .flex-col > .custom-input',
+        systalic:':nth-child(6) > .flex-col > .custom-input',
+        diastalic:':nth-child(7) > .flex-col > .custom-input',
+        bp:':nth-child(7) > .flex-col > .custom-input',
+        pulseRate:':nth-child(9) > .flex-col > .custom-input',
+        respiratoryRate:':nth-child(10) > .flex-col > .custom-input',
+        oxygen:':nth-child(11) > .flex-col > .custom-input',
+        muac:":nth-child(12) > .flex-col > .custom-input",
+        muacScore:':nth-child(13) > .flex-col > .custom-input',
+        abdominal:':nth-child(14) > .flex-col > .custom-input',
+        headCircum:':nth-child(15) > .flex-col > .custom-input',
+        hcScore:':nth-child(16) > .flex-col > .custom-input',
+        Note:':nth-child(17) > .flex-col > .custom-input'
+    }
+}
